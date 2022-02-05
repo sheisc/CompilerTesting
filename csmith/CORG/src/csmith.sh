0a4816e680d53676ed4bbc866debf7e1b6135e77
sheisc <sheisc@163.com>
2022-02-05 13:23:25 +1100

    INIT CompilerTesting

---
+++ csmith/CORG/src/csmith.sh
@@ -0,0 +1,4 @@
+export CSMITH_HOME=/home/iron/github/csmith/csmith.installed
+export LLVM_INSTALL_PATH=/home/iron/github/SVF10.0/llvm-10.0.0.obj
+export PATH=$LLVM_INSTALL_PATH/bin:$CSMITH_HOME/bin:$PATH
+
---
+++ src/csmith_driver.py
@@ -0,0 +1,6 @@
+from build_run import differ_testing
+
+if __name__ == '__main__':
+    target_name = "test_case"
+    while True:
+        differ_testing(target_name)
